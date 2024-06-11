7412f9213820bb4edc7f10294ca7485a78fdb034
zzezl <maxmandarin29@gmail.com>
2024-06-11 16:42:59 +0300

    eet

--- js/script.js
+++ js/script.js
@@ -16,9 +16,9 @@ createContent(div, null)
 const input = document.querySelector('.search')
 
 input.addEventListener('input', e => {
+    e.preventDefault();
     const { target } = e
     const items = document.querySelectorAll('.item')
-
     
     if (target.value.toLowerCase().trim() !== '') {
         console.dir(target.value)
@@ -27,24 +27,19 @@ input.addEventListener('input', e => {
         console.dir(items)
     } else {
         if (target.value.toLowerCase().trim() === ""){
+            document.querySelector('.notfound').classList.add('inv')
             createContent(div)
             return
         } else {
-            div.innerHTML = `
-            <div class="notfound">
-            <h3>Character not found!</h3>
-            <h5>Try another character or another okun'</h4>
-    </div>
-            `
+            div.innerHTML = null
+            document.querySelector('.notfound').classList.remove('inv')
         }
     }
 })
 
 
-document.addEventListener('scroll', e => {
-    if(window.scrollY >= 1000) {
-        console.dir('start')
-        
-    }
-    document.removeEventListener()
-})
+// document.addEventListener('scroll', e => {
+//     if(window.scrollY >= 1000) {
+//         console.dir('start')
+//     }
+// })
